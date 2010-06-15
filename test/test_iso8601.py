2c68758032d072138a51a2b5fd6b7beca35664ce
Andrew Kuchling <amk@amk.ca>
2010-06-14 21:51:55 -0400

    Convert to unittest

--- test/test_iso8601.py
+++ test/test_iso8601.py
@@ -1,11 +1,21 @@
 # Test the modules in the utils/ subpackage
 
+import unittest
+
 from qel import iso8601
 
-for dt in ['1998', '1998-06', '1998-06-13',
-           '1998-06-13T14:12Z',
-           '1998-06-13T14:12:30Z',
-           '1998-06-13T14:12:30.2Z'
-           ]:
-    date = iso8601.parse( dt )
-    print iso8601.tostring( date )
+def tostr(dt):
+    date = iso8601.parse(dt)
+    return iso8601.tostring(date)
+
+class TestISO8601(unittest.TestCase):
+    def test_basic(self):
+        self.assertEqual(tostr('1998'), '1998-01-01T00:00Z')
+        self.assertEqual(tostr('1998-06'), '1998-06-01T00:00Z')
+        self.assertEqual(tostr('1998-06-13'), '1998-06-13T00:00Z')
+        self.assertEqual(tostr('1998-06-13T14:12Z'), '1998-06-13T14:12Z')
+        self.assertEqual(tostr('1998-06-13T14:12:30Z'), '1998-06-13T14:12:30Z')
+        self.assertEqual(tostr('1998-06-13T14:12:30.2Z'), '1998-06-13T14:12:30Z')
+
+if __name__ == "__main__":
+    unittest.main()
