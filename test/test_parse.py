5a1eb0c4583e82a119eb4846cd23e6d84ba4d77d
Andrew Kuchling <amk@amk.ca>
2013-08-03 15:38:37 -0400

    Use .assertEqual() instead of assertEquals()

--- test/test_parse.py
+++ test/test_parse.py
@@ -22,7 +22,7 @@ class QELParseTest(unittest.TestCase):
         # Empty file
         xml = self.xml_header + "<quotations></quotations>"
         input = StringIO(xml)
-        self.assertEquals(parse.parse(input), [])
+        self.assertEqual(parse.parse(input), [])
 
         # Empty quotation
         xml = self.xml_header + ("<quotations><quotation>"
@@ -43,9 +43,9 @@ class QELParseTest(unittest.TestCase):
         input = StringIO(xml)
         coll = parse.parse(input)
 
-        self.assertEquals(coll.title, 'Test Title')
-        self.assertEquals(coll.editor, 'Me')
-        self.assertEquals(coll.description, 'More text')
+        self.assertEqual(coll.title, 'Test Title')
+        self.assertEqual(coll.editor, 'Me')
+        self.assertEqual(coll.description, 'More text')
 
     def test_parse(self):
         "Check parsing of an actual quotation"
@@ -61,23 +61,23 @@ class QELParseTest(unittest.TestCase):
         coll = parse.parse(input)
 
         qt = coll[0]
-        self.assertEquals(qt.id, 'qtid')
-        self.assertEquals(qt.date, '2000-01-01')
-        self.assertEquals(qt.type, ['funny', 'silly'])
+        self.assertEqual(qt.id, 'qtid')
+        self.assertEqual(qt.date, '2000-01-01')
+        self.assertEqual(qt.type, ['funny', 'silly'])
 
         # Check first and second paragraph
-        self.assertEquals(qt.text[0][0].as_text(), 'para1')
-        self.assertEquals(qt.text[1][0].as_text(), '*bold*')
+        self.assertEqual(qt.text[0][0].as_text(), 'para1')
+        self.assertEqual(qt.text[1][0].as_text(), '*bold*')
 
         # Test third paragraph contains a Break
         self.assertTrue(isinstance(qt.text[2][1], quotation.Break))
 
         # Check author, source, and notes
-        self.assertEquals(qt.author.as_text(), 'a1')
-        self.assertEquals(qt.author.type, ['a1','a2'])
-        self.assertEquals(qt.source.as_text(), 's1')
-        self.assertEquals(qt.source.type, ['s1','s2'])
-        self.assertEquals(qt.note[0][0].as_text(), 'n1')
+        self.assertEqual(qt.author.as_text(), 'a1')
+        self.assertEqual(qt.author.type, ['a1','a2'])
+        self.assertEqual(qt.source.as_text(), 's1')
+        self.assertEqual(qt.source.type, ['s1','s2'])
+        self.assertEqual(qt.note[0][0].as_text(), 'n1')
 
     def test_simplify(self):
         "Check the simplify() internal function: 6"
@@ -86,25 +86,25 @@ class QELParseTest(unittest.TestCase):
         # Invariant
         L = [Text('abc'), EmphasizedText('cde')]
         L = parse.simplify(L)
-        self.assertEquals(L[0].as_text(), 'abc')
-        self.assertEquals(L[1].as_text(), '*cde*')
+        self.assertEqual(L[0].as_text(), 'abc')
+        self.assertEqual(L[1].as_text(), '*cde*')
 
         # Test merging of chunks
         L = [Text('cde'), Text('abc')]
         L = parse.simplify(L)
-        self.assertEquals(L[0].as_text(), 'cdeabc')
+        self.assertEqual(L[0].as_text(), 'cdeabc')
 
         # Test simple whitespace trimming
         L = [Text(''), Text(' abc '), EmphasizedText('cde')]
         L = parse.simplify(L)
-        self.assertEquals(L[0].as_text(), 'abc ')
+        self.assertEqual(L[0].as_text(), 'abc ')
 
         # Test more complicated whitespace trimming.
         # This should insert a Text chunk in the middle.
         L = [EmphasizedText('abc '), EmphasizedText(' cde')]
         L = parse.simplify(L)
-        self.assertEquals(L[0].as_text(), '*abc*')
-        self.assertEquals(L[1].as_text(), ' ')
+        self.assertEqual(L[0].as_text(), '*abc*')
+        self.assertEqual(L[1].as_text(), ' ')
 
 if __name__ == "__main__":
     unittest.main()
