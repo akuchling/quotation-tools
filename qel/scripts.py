afd48fc46176ee2a8eb44d8db1ecdf099103fbb3
Andrew Kuchling <amk@amk.ca>
2013-08-24 22:50:15 -0400

    Add --html-pages switch

--- qel/scripts.py
+++ qel/scripts.py
@@ -2,7 +2,7 @@
 qel.scripts -- Entry points for the quotation-tools scripts
 """
 
-import re, time
+import os, re, time
 from . import quotation
 
 class DuplicateIDException(Exception):
@@ -59,3 +59,69 @@ def merge(*collections, strict=True, generate=False):
                 ids.add(qt.id)
 
     return newcoll
+
+
+def _divide_into_columns(qtcoll):
+    # Group the quotations into a set of columns of roughly equal length.
+    # We'll count the number of characters in the HTML as an estimator of
+    # its length.
+    quote_list = [qt.as_html() for qt in qtcoll]
+    quote_list = [(len(html), html) for html in quote_list]
+
+    # Figure out the total size; we'll try to have 5K of material per column.
+    total_size = sum(size for (size, html) in quote_list)
+    target_column_size = 5 * 1024 # Size of 10K
+
+    # Divide the quotes up into lists of columns.
+    current_size = 0
+    column_list = [ [] ]
+    for size, html in quote_list:
+        column_list[-1].append(html)
+        current_size += size
+        if current_size > target_column_size:
+            current_size = 0
+            column_list.append( [] )
+
+    # If last column is empty, remove it.
+    if len(column_list[-1]) == 0:
+        del column_list[-1]
+
+    # Ensure there is always an even number of column lists.
+    if (len(column_list) % 2) == 1:
+        column_list.append([])
+
+    return column_list
+
+def write_html_dir(dir, qtcoll):
+    """Export a quotation file as a set of HTML pages, using a fancy template.
+    """
+    from qel.data import HTML_TEMPLATE
+
+    column_list = _divide_into_columns(qtcoll)
+
+    page_num = 1
+    while len(column_list):
+        if page_num == 1:
+            fn = 'index.html'
+        else:
+            fn = '{}.html'.format(page_num)
+        path = os.path.join(dir, fn)
+
+        last_attr = ('data-last=""' if len(column_list) == 0 else '')
+
+        col1 = column_list.pop(0)
+        col2 = column_list.pop(0)
+
+        page = HTML_TEMPLATE.format(page_num=page_num,
+                                    last_attr=last_attr,
+                                    col1='\n'.join(col1),
+                                    col2='\n'.join(col2),
+                                    title="Quotation Collection",
+                                )
+
+        with open(path, 'wt', encoding='latin-1') as f:
+            f.write(page)
+
+        page_num += 1
+
+    print(page_num-1, 'pages written')
